62b288b603f6ae060b8aa5ae8cf9e7ec66b0554d
boxmars <2803726925@qq.com>
2021-02-28 03:54:42 +0800

    load_seeting

---
+++ main.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+API_TOKEN=''
+API_SERVER=''
+PORT=0
+
+def load_seeting():
+    try:
+        with open(os.path.join(DIR,"config.json")) as config_json:
+            config=json.load(config_json)
+    except FileNotFoundError:
+        print("No Config")
+    return config["API_TOKEN"], config["API_SERVER"], config["PORT"]
