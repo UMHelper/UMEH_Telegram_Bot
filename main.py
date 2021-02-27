c7c126e4e2234125929d5c2400b224ee311001a0
boxmars <2803726925@qq.com>
2021-02-28 04:38:25 +0800

    getUpdates

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import json
 import os
+import requests
 
 DIR = os.path.dirname(os.path.abspath(__file__))
-
-API_TOKEN = ''
-API_SERVER = ''
-PORT = 0
+API_SERVER = "https://api.telegram.org/bot"
 
 
 def load_setting():
@@ -14,4 +12,20 @@ def load_setting():
             config = json.load(config_json)
     except FileNotFoundError:
         print("No Config")
-    return config["API_TOKEN"], config["API_SERVER"], config["PORT"]
+    return config["API_TOKEN"], config["UMEH_SERVER"], config["PORT"]
+
+
+API_TOKEN, UMEH_SERVER, PORT = load_setting()
+
+
+def get_update(offset=0):
+    r=requests.get(API_SERVER+API_TOKEN+"/getUpdates?offset="+str(offset))
+    r.json()
+
+
+def main():
+    get_update()
+
+
+if __name__ == '__main__':
+    main()
