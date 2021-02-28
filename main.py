ab7da67db4c4c3b0659c9fae03c6eebbadf068bb
boxmars <2803726925@qq.com>
2021-02-28 16:04:54 +0800

    shou gong la

--- main.py
+++ main.py
@@ -13,20 +13,61 @@ def load_config():
             config = json.load(config_json)
     except FileNotFoundError:
         print("No Config")
-    return config["API_TOKEN"], config["UMEH_SERVER"], config["PORT"], config["MESSAGE_ID"], config
+    return config["API_TOKEN"], config["UMEH_SERVER"], config["PORT"], config
 
 
-API_TOKEN, UMEH_SERVER, PORT, MESSAGE_ID, CONFIG_DIC = load_config()
+API_TOKEN, UMEH_SERVER, PORT, CONFIG_DIC = load_config()
 
 
-def config_update( message_id):
+def config_update():
     try:
         with open(os.path.join(DIR, "config.json"), 'w+') as config_json:
-            CONFIG_DIC["MESSAGE_ID"] = message_id
             json.dump(CONFIG_DIC, config_json)
             config_json.close()
     except FileNotFoundError:
         print("No Config")
+
+
+messages = []
+
+
+def process_message():
+    '''
+            :var messages List
+                Message list beginning with the latest message of last update
+        '''
+
+    '''
+            Example:
+            [
+                {
+                    'update_id' : 414331188,
+                    'message' : {
+                        'message_id'  4，
+                        ‘from’ : {
+                            'id' : 81632974,
+                            'is_bot' : false
+                            'first_name' : 'Kou',
+                            'last_name' : 'Mei',
+                            'username': 'KouMei',
+                            'language_code' : 'zh-hans
+                        },
+                        'chat' : {
+                            'id' : 81632974,
+                            'is_bot' : false
+                            'first_name' : 'Kou',
+                            'last_name' : 'Mei',
+                            'username': 'KouMei',
+                            'type' : 'private'
+                        },
+                        date : 1614458067,
+                        text : 'text'
+                    }
+                },
+                {},{},{}...
+            ]
+    '''
+    pass
 
 
 '''
@@ -35,8 +76,11 @@ def config_update( message_id):
 '''
 
 
-def get_updates(offset=0):
-    r = requests.get(API_SERVER + API_TOKEN + "/getUpdates?offset=" + str(offset))
+
+
+def get_updates():
+    path = API_SERVER + API_TOKEN + "/getUpdates?offset=" + str(CONFIG_DIC["MESSAGE_ID"])
+    r = requests.get(path)
     '''
         :var messages List
             Message list beginning with the latest message of last update
@@ -72,12 +116,15 @@ def get_updates(offset=0):
                 {},{},{}...
             ]
     '''
+    global messages
     messages = r.json()["result"]
-    latest_message_id = messages[len(messages) - 1]['update_id']
+    CONFIG_DIC["MESSAGE_ID"] = messages[len(messages) - 1]['update_id']
+    config_update()
 
-# def main():
-#     get_update()
-#
-#
-# if __name__ == '__main__':
-#     main()
+
+def main():
+    get_updates()
+
+
+if __name__ == '__main__':
+    main()
