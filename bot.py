09dc2947b28085f1cfd97d31659827be1d646825
boxmars <2803726925@qq.com>
2021-03-05 14:43:10 +0800

    some necessary methods

--- bot.py
+++ bot.py
@@ -3,10 +3,10 @@ import telegram
 
 class UmehBot:
 
-    def __init__(self, token, message_id):
+    def __init__(self, token: str, message_id: int):
         self.bot = telegram.Bot(token=token)
         self.MESSAGE_ID = message_id
-        self.message = []
+        self.messages = []
 
     def get_updates(self):
         '''
@@ -45,13 +45,31 @@ class UmehBot:
                     }
                 }
                 '''
-        self.message = self.bot.get_updates(offset=self.MESSAGE_ID)
+        self.messages = self.bot.get_updates(offset=self.MESSAGE_ID)
 
     def update_message_id(self):
-        self.MESSAGE_ID = self.message[len(self.message) - 1]['update_id']
+        self.MESSAGE_ID = self.messages[len(self.messages) - 1]['update_id']
 
-    def get_message_id(self):
+    def get_message_id(self) -> int:
         return self.MESSAGE_ID
 
-    def send_message(self, chat_id: int = 0, text: str = ''):
+    def send_message(self, chat_id: int, text: str, parse_mode: str, reply_markup: telegram.ReplyMarkup):
+        return self.bot.send_message(chat_id=chat_id, text=text, parse_mode=parse_mode, reply_markup=reply_markup)
+
+    def is_text_message(self, message: telegram.Update) -> bool:
         pass
+
+    def is_command_message(self, message: telegram.Update) -> bool:
+        pass
+
+    def is_course_code(self, message: telegram.Update) -> bool:
+        pass
+
+    def if_course_exist(self, result: dict) -> bool:
+        pass
+
+    def get_course_info(self, code: str) -> dict:
+        pass
+
+    def send_course_info(self, chat_id: int, text: str, parse_mode: str, reply_markup: telegram.ReplyMarkup):
+        return self.send_message(chat_id, text, parse_mode, reply_markup)
